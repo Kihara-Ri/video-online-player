7884b56a8c1a06c8e04decaeb64d1d1a3318bba1
kihara <limuyuan3650@foxmail.com>
2025-01-23 03:00:48 +0000

    修改端口和可访问权限

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // 监听所有网络接口
+    port: 3000, // 服务开放端口
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
